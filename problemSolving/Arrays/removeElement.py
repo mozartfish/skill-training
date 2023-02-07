705fba720b580a93fe9b1b2ffd03aba2c0936cda
Pranav Rajan <rajan.pranav320@gmail.com>
2023-02-07 15:08:03 -0700

    Solve static array problems

---
+++ problemSolving/Arrays/removeElement.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i, k = 0, 0
+        while i < len(nums):
+            if nums[i] == val:
+                i += 1
+            else:
+                nums[k] = nums[i]
+                i += 1
+                k += 1
+        return k
+        
