1163e0ffafae0a395cf7fb03001764ba33ec6cba
Pranav Rajan <rajan.pranav320@gmail.com>
2023-02-08 23:11:33 -0700

    Add error checking to go module

--- golang/goModules/goModules.go
+++ golang/goModules/goModules.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	// Get a greeting message and print it
-	message := greetings.Hello("James Bond")
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// IF no error was returned, print the returned message to the console
 	fmt.Println(message)
 }
