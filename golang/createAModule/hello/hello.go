36a8dccaac83fccf239bd57b4597b29e0687ff88
Pranav Rajan <rajan.pranav320@gmail.com>
2023-02-09 18:06:27 -0700

    Introduction to creating go modules, testing and building go executable programs

---
+++ golang/createAModule/hello/hello.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+import "example.com/greetings"
+import "log"
+
+func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message
+	// message, err := greetings.Hello("")
+	// message, err := greetings.Hello("James Bond")
+
+	// A slice of names.
+	names := []string{"James Bond", "Bruce Wayne", "Daft Punk"}
+
+	// Request greeting message for the names
+	messages, err := greetings.Hellos(names)
+
+	// If an error was returned, print it to the console
+	// and exit the program
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console
+	fmt.Println(messages)
+}
